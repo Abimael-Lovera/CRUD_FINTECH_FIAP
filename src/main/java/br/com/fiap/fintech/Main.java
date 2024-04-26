79900e7a3c6467102ee82274fc0f15711e32ed70
Abimael Lovera <abimaellovera@gmail.com>
2024-04-25 23:16:25 -0300

    comentando app

--- src/main/java/br/com/fiap/fintech/Main.java
+++ src/main/java/br/com/fiap/fintech/Main.java
@@ -5,7 +5,12 @@ import br.com.fiap.fintech.controller.*;
 public class Main {
     public static void main(String[] args)  {
         System.out.println("Inicializando aplicação...");
-        try {
+        /** Todos os Controllers tem um método iniciarAplicacao() que cria um objeto e salva no banco de dados
+         e outro método deletarTodos() que deleta todos os objetos do banco de dados
+        Após relacionar a Tabela usuario com investimentos, despesas e receitas,
+         não é mais possível deletar todos os regristros da Tabela Usuarios
+        **/
+         try {
             var usuarioController = new UsuarioController();
 //            usuarioController.deletarTodos();
             usuarioController.iniciarAplicacao();
